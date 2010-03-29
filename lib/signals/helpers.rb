1391ce13e3d16c7df6e6cc9dccfa33b98b2a6774
Florent Ruard-Dumaine <florent@styx.(none)>
2010-03-29 17:14:15 +0000

    use url_for(:root) for prefix signal check from JS, to use relative_url_root but other prefix configured (like locale in url

--- lib/signals/helpers.rb
+++ lib/signals/helpers.rb
@@ -4,7 +4,7 @@ module ActionView::Helpers
 		def activeSignalsJavascript(options = nil)
 			options = options || {}
 			javascript_include_tag('signals', :plugin => 'signals')+
-				'<script type="text/javascript">Event.observe(window, \'load\', function(e){Signals.init(\''+controller.relative_url_root+'\', {'+options.collect {|key, value| "#{key.to_s}: #{value}"}.join(', ')+'});});</script>'
+				'<script type="text/javascript">Event.observe(window, \'load\', function(e){Signals.init(\''+url_for(:root)+'\', {'+options.collect {|key, value| "#{key.to_s}: #{value}"}.join(', ')+'});});</script>'
 		end
 	end
 end
